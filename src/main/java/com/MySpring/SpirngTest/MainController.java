3807566407e73556a0a27682623fa438862197ff
BaeSangUn <zyxtkddns@naver.com>
2022-10-04 18:33:11 +0900

    점프 투 자바 복습

---
+++ src/main/java/com/MySpring/SpirngTest/MainController.java
@@ -0,0 +1,13 @@
+package com.MySpring.SpirngTest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class MainController {
+    @RequestMapping("/sbb2")
+    @ResponseBody
+    public String index(){
+        return "index";
+    }
+}
